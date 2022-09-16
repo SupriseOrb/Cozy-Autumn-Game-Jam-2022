5f064bc81cf06038461158e24c37073594637edf
kristenmc <kriscampbell1999@gmail.com>
2022-09-16 12:58:07 -0700

    Added UI sounds to Wwise, created RTPC values for volume

--- CozyAutumnJam_2022/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ CozyAutumnJam_2022/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -18,6 +18,11 @@ namespace AK
         static const AkUniqueID PLAY_CARDSHUFFLE = 3192869359U;
         static const AkUniqueID PLAY_DECKSWAP = 4280915480U;
         static const AkUniqueID PLAY_TESTSOUND = 2752533807U;
+        static const AkUniqueID PLAY_UIBACK = 1224305705U;
+        static const AkUniqueID PLAY_UIPAUSE = 1830907318U;
+        static const AkUniqueID PLAY_UIRESUME = 235035089U;
+        static const AkUniqueID PLAY_UISCROLL = 3312925211U;
+        static const AkUniqueID PLAY_UISELECT = 3703192648U;
     } // namespace EVENTS
 
     namespace STATES
@@ -40,6 +45,14 @@ namespace AK
 
     } // namespace STATES
 
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID ALLVOLUME = 2731583604U;
+        static const AkUniqueID AMBIANCEVOLUME = 365633811U;
+        static const AkUniqueID MUSICVOLUME = 2346531308U;
+        static const AkUniqueID SFXVOLUME = 988953028U;
+    } // namespace GAME_PARAMETERS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
@@ -48,7 +61,10 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIANCE = 2981377429U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES

adf52b8d4d73744a0ef309ac054e6d1c8dd4e110
Molina <molinakmey@gmail.com>
2022-09-22 17:26:05 -0700

    Add loading screen

--- CozyAutumnJam_2022/Assets/Scripts/Menu/Menu.cs
+++ CozyAutumnJam_2022/Assets/Scripts/Menu/Menu.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     [SerializeField] private GameObject _settingsCanvas;
-    [SerializeField] protected BoolVariable _isMusicPlaying; 
+    [SerializeField] protected BoolVariable _isMusicPlaying;
+    [SerializeField] private GameObject _loadingScreen;
+    [SerializeField] private Slider _loadingBar;
 
     protected void LoadScene(int sceneIndex)
     {
         //Play regular button sound
         AkSoundEngine.PostEvent("Play_UISelect", this.gameObject);
-        SceneManager.LoadScene(sceneIndex);
+        //SceneManager.LoadScene(sceneIndex);
+        StartCoroutine(LoadSceneAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadSceneAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        _loadingBar.interactable = false;
+        _loadingScreen.SetActive(true);
+        while(!operation.isDone)
+        {
+            _loadingBar.value = operation.progress;
+            yield return null;
+        }
     }
 
     protected void SetCanvasVisibility(Canvas canvas, bool canSee)
